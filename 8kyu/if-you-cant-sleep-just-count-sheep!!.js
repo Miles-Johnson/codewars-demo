0cb08e5e97018d0d8ba0a3caaa97722adada3af6
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-10-26 13:15:59 -0800

    Trying new method of adding the psuedo code as the answer to anki for code wars, and including psuedo code with solutions

---
+++ 8kyu/if-you-cant-sleep-just-count-sheep!!.js
@@ -0,0 +1,14 @@
+//My Solution
+
+function countSheep(n){
+   let str = ""
+    for( let i = 1; i <= n; i++){
+      str += `${i} sheep...`}
+    return str;
+  }
+
+
+  //variable to hold the string is declared and assigned
+  //for loop is run however many times the input says to
+   //every time the loop runs, it concatinates the following string into the variable 
+   //variable is returned
