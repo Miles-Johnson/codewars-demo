7f3786e42b58f0c708c7fc2d3ae70f7d75cd671f
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-10-26 13:40:06 -0800

    added power of two with psuedo code

---
+++ 8kyu/powers-of-two.js
@@ -0,0 +1,16 @@
+//My Solution
+
+
+function powersOfTwo(n){
+   let result = []
+   for(let i = 0; i<= n; i++){
+    result.push(Math.pow(2,i))
+   }
+   return result
+  }
+
+
+//create a variable to hold the array
+//run a loop for however many times the input parameter is for
+//on each iteration of the loop, place the result of 2^i into the array
+//return the result
