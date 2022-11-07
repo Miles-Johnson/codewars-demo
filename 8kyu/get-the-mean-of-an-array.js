883fd9b7135bc0c93bd71064edff71a11d58ff2c
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-11-07 11:15:57 -0900

    Added get the mean codewars

---
+++ 8kyu/get-the-mean-of-an-array.js
@@ -0,0 +1,24 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+// Arrays
+// Algorithms
+
+
+
+// My Solution
+
+function getAverage(marks){
+    let sum = marks.reduce((a,b)=> (a+b))
+   let mean = sum/marks.length
+    return Math.floor(mean)
+   }
+
+
+//    Other solutions
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+  }
