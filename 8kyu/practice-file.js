d253866ab4c1b75c2a81d9503bb8a48eeb616962
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-11-12 13:19:16 -0900

    Practice folder changes

--- 8kyu/practice-file.js
+++ 8kyu/practice-file.js
@@ -1,3 +1,14 @@
-function returnNumber(arr,n=1){
-    return arr.slice(0,n)
+
+
+//take a string
+//split the string into an array
+//find the index of all the elements of that array
+//identify which elements are capitalized
+//return only the capitalized elements
+
+
+
+function word(str){
+str.match(/[A-Z]/g).forEach((e,i)=> return i )
+
 }
