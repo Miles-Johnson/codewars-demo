dc466ec1baa4bd08a08b7ab6a11058b69f150480
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-11-23 12:25:04 -0900

    New 8kyu

---
+++ 8kyu/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,15 @@
+// Convert number to reversed array of digits
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example(Input => Output):
+
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+
+
+//My Solutions
+
+function digitize(n) {
+    return Array.from(String(n), Number).reverse()
+      }
