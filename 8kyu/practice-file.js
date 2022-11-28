2be913051ee07989794b94715dc4942fb4ef41a4
Miles-Johnson <bluehaakonjohnson@gmail.com>
2022-11-28 14:49:25 -0900

    Changes

--- 8kyu/practice-file.js
+++ 8kyu/practice-file.js
@@ -1,9 +1,4 @@
-
-function countTheMonkeys(n){
-  let newArr = []
-  for(let i = 1; i<= n; i += 1){
- newArr.push(i)
-  }
-   return newArr
-
+function countPositivesSumNegatives(input) {
+ let newArr = []
+ if
 }
